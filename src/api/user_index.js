068049508ee0d69e326b51131abe71d9ab9ab857
显哥 <mojiexinage199427@136.com>
2020-03-31 16:40:17 +0800

    添加登陆的接口方法

---
+++ src/api/user_index.js
@@ -0,0 +1,11 @@
+// 1.引入axios
+import axios from '@/utils/myaxios.js'
+
+// 登录
+export const login = (data) => {
+  return axios({
+    url: 'login',
+    method: 'post',
+    data
+  })
+}
---
+++ src/utils/mayaxios.js
@@ -0,0 +1,7 @@
+// 这个文件封装与axios相关的所有配置
+import axios from 'axios'
+// 设置基准路径
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+
+// 导出
+export default axios
